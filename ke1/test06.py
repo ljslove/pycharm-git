10cead55675b77872ae0b4159b51287fa14abdf2
ljslove <lijingshan.love@163.com>
2019-07-05 22:41:54 +0800

    commit test6

---
+++ ke1/test06.py
@@ -0,0 +1,19 @@
+
+
+
+
+def add(a,b):
+    c=0
+    if (isinstance(a,int) or isinstance(a,float)) and (isinstance(b,int) or isinstance(b,float)):
+        c=a+b
+    else:
+        print("请输入整数或者小数")
+    print(c)
+
+add(1,34)
+add(12.4,13.4)
+#add("454",233)
+#add("889","899")
+add(454,"7889")
+
+
