6f99970ad96506bb2dfa2385821fbfd630f68bbe
ljslove <lijingshan.love@163.com>
2019-07-05 23:48:01 +0800

    commit test2-1

---
+++ ke1/test02.py
@@ -0,0 +1,26 @@
+
+from selenium import  webdriver
+import time
+
+driver=webdriver.Chrome()
+
+
+driver.get("http://www.baidu.com")
+time.sleep(5)
+driver.get("http://www.hao123.com")
+#返回上一页
+driver.back()
+
+
+
+time.sleep(5)
+#返回下一页
+driver.forward()
+
+time.sleep(5)
+driver.refresh()
+
+time.sleep(5)
+driver.quit()
+
+print("hoigjio")
